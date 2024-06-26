8f8d52043e2a0d550cbb1f75ba7bac136047ecdd
Sami Alabed <sami.alabed.94@gmail.com>
2024-06-26 12:22:23 +0200

    DONE!

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,59 @@
+package se.lexicon;
+
+
+import se.lexicon.model.Book;
+import se.lexicon.model.Person;
+
+public class App {
+    public static void main(String[] args) {
+        // todo: needs completion
+
+        // Create books.
+        Book tlotr = new Book("The Lord of the Rings", "J.R.R. Tolkien");
+        Book hp = new Book("Harry Potter", "J.K. Rowling");
+
+        // Create a person instance
+        Person person = new Person("Sami", "Alabed");
+        // Display person information.
+        String personInformation = person.getPersonInformation();
+        System.out.println(personInformation);
+
+
+        // Loan a book to the person
+        person.loanBook(tlotr);
+        // Display person information after borrowing a book
+        System.out.println("\nAfter loaning the book, the person should have one book!");
+        personInformation = person.getPersonInformation();
+        System.out.println(personInformation);
+
+        // Display book information after borrowing a book
+        System.out.println("\nAfter loaning the book, the available should be false!");
+        String bookInformation = tlotr.getBookInformation();
+        System.out.println(bookInformation);
+
+        // Loan another book to the person.
+        person.loanBook(hp);
+
+
+        // Display the books that the person has borrowed.
+        System.out.println("\nBooks that the person has borrowed:");
+        for (Book book : person.getBooks()) {
+            System.out.println(book.getBookInformation());
+        }
+
+        // Return the borrowed book
+        person.returnBook(tlotr);
+        // Display person information after returning the book
+        personInformation = person.getPersonInformation();
+        System.out.printf(personInformation);
+        // Display book information after borrowing a book
+        System.out.println("After returning the tlotr, the available should be true!");
+        bookInformation = tlotr.getBookInformation();
+        System.out.println(bookInformation);
+
+
+
+
+    }
+
+}
